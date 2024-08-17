1d1499521ed43e023e0db80c1ed8249a28fb2671
poemota <142114334+poeMota@users.noreply.github.com>
2024-08-17 17:24:05 +0300

    add python version check

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+
 from src.Tiles import TilesRefsManager
 from src.UI import Window
 
 
 if __name__ == '__main__':
+    if sys.version_info.major < 3:
+        print("You need python 3+ version to run this app")
+        sys.exit(1)
+
     _tilesRefsMan = TilesRefsManager()
     _tilesRefsMan.getColorRefs()
 
