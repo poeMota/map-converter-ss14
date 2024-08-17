4b5d34c14be3ab9095139909d33c84b78f7fb18d
poemota <142114334+poeMota@users.noreply.github.com>
2024-08-17 17:51:28 +0300

    add install scripts

---
+++ install.bat
@@ -0,0 +1,17 @@
+@echo off
+
+where python >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo ERROR: Python not installed
+    exit /b 1
+)
+
+where pip >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo ERROR: pip not installed
+    exit /b 1
+)
+
+git submodule update
+pip install -r requirements.txt
+
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/usr/env bash
+if ! command -v python3 &> /dev/null; then
+    echo "ERROR: Python not installed"
+    exit 1
+fi
+
+if ! command -v pip3 &> /dev/null; then
+    echo "ERROR: pip not installed"
+    exit 1
+fi
+
+
+git submodule update
+pip install -r requirements.txt
+
