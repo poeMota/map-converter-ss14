c5d067d32f28e40553146d111d3bc90235b9572c
poemota <142114334+poeMota@users.noreply.github.com>
2024-08-17 20:42:27 +0300

    fix popup position

--- src/UI/popup.py
+++ src/UI/popup.py
@@ -4,10 +4,12 @@ import customtkinter as ctk
 class PopupWindow(ctk.CTkToplevel):
     def __init__(self, master, title: str, message: str, width=300, height=150, buttlonLabel: str = "Close"):
         super().__init__(master)
+
         self.title(title)
-        self.geometry(f"{width}x{height}")
+        self.center_window(height, width)
 
         self.attributes("-topmost", True)
+        self.resizable(False, False)
 
         # Message label
         self.label = ctk.CTkLabel(self, text=message, wraplength=250)
@@ -18,6 +20,20 @@ class PopupWindow(ctk.CTkToplevel):
         self.close_button.pack(pady=10)
 
         self.after(1, self.grab_set) # the window may not have time to initialise
+
+
+    def center_window(self, height, width):
+        self.update_idletasks()
+
+        parent_x = self.master.winfo_x()
+        parent_y = self.master.winfo_y()
+        parent_width = self.master.winfo_width()
+        parent_height = self.master.winfo_height()
+
+        x = parent_x + (parent_width // 2) - (width // 2)
+        y = parent_y + (parent_height // 2) - (height // 2)
+
+        self.geometry(f'{width}x{height}+{x}+{y}')
 
 
     def close_window(self):
