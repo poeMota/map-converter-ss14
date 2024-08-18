d01f011e3a6b2e7f5723e59870c4ef0b4723f7c9
poemota <142114334+poeMota@users.noreply.github.com>
2024-08-18 15:49:58 +0300

    now TilesRefsManager can search from folders

--- src/Tiles/tileRefManager.py
+++ src/Tiles/tileRefManager.py
@@ -1,4 +1,4 @@
-import os
+from os import listdir, path
 from PIL import Image
 
 import src.yaml as yaml
@@ -9,30 +9,39 @@ class TilesRefsManager:
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(TilesRefsManager, cls).__new__(cls)
-            cls.tilesDataPath = os.path.dirname(__file__) + '/data/'
-            cls.texturesPath = os.path.dirname(__file__) + '/../../'
-
-            cls.prototypes = []
-            cls.tileRefs = {} # TileName: tileID
-            cls.colorRefs = {} # color: TileName
-
-            cls.getPrototypes(cls)
-            cls.getTileRefs(cls)
+            cls.instance._initialized = False
         return cls.instance
 
 
-    def getPrototypes(self):
-        for filename in os.listdir(self.tilesDataPath):
+    def __init__(self):
+        if not self._initialized:
+            self.tilesDataPath = path.dirname(__file__) + '/../../Tiles/'
+            self.texturesPath = path.dirname(__file__) + '/../../'
+
+            self.prototypes = []
+            self.tileRefs = {} # TileName: tileID
+            self.colorRefs = {} # color: TileName
+
+            self.getPrototypes(self.tilesDataPath)
+            self.getTileRefs()
+
+            self._initialized = True
+
+
+    def getPrototypes(self, dirPath):
+        for filename in listdir(dirPath):
             if '.yml' in filename:
-                data = yaml.read(self.tilesDataPath + filename)
+                data = yaml.read(dirPath + filename)
                 if type(data) is list:
                     [self.prototypes.append(i) for i in data]
                 else:
                     self.prototypes.append(data)
+            elif path.isdir(dirPath + filename):
+                self.getPrototypes(f"{dirPath}{filename}/")
 
 
     def getTileRefs(self):
-        tileRefs = {"Space": 0} # Space tile should bo only 0 id
+        tileRefs = {"Space": 0} # Space tile should be only 0 id
 
         for proto in self.prototypes:
             if (proto["type"] == "tile"
