3a4aa324c21f5744c3ac99b0d2a5b04f852a0bb1
poemota <142114334+poeMota@users.noreply.github.com>
2024-08-20 09:35:55 +0300

    move config to app folder

--- src/Config/config.py
+++ src/Config/config.py
@@ -1,19 +1,9 @@
-import os
+from os import path
 from pathlib import Path
-import platform
 
 
-def getConfigPath(folderName="map-converter-ss14") -> Path:
-    system = platform.system()
-
-    if system == "Windows":
-        configDir = os.getenv('APPDATA')
-    elif system in ["Linux", "Darwin"]:  # "Darwin" for macOS
-        configDir = Path.home() / ".config"
-    else:
-        raise NotImplementedError(f"Unsupported operating system: {system}")
-
-    configPath = Path(configDir) / folderName
+def getConfigPath() -> str:
+    configPath = Path(path.dirname(__file__) + '/../../.config/')
     configPath.mkdir(parents=True, exist_ok=True)
 
     return configPath
