415d0d562894c56ccd7e577324ba6468aeb61627
poemota <142114334+poeMota@users.noreply.github.com>
2024-09-05 14:36:56 +0300

    update install.sh for Arch based Linux

--- install.sh
+++ install.sh
@@ -4,13 +4,26 @@ if ! command -v python3 &> /dev/null; then
     exit 1
 fi
 
-if ! command -v pip3 &> /dev/null; then
-    echo "ERROR: pip not installed"
-    exit 1
-fi
-
-
 git submodule init
 git submodule update
-pip install -r requirements.txt
+
+if ! command -v pacman &> /dev/null; then
+   if ! command -v pip3 &> /dev/null; then
+       echo "ERROR: pip not installed"
+       exit 1
+   fi
+
+   pip install -r requirements.txt
+else
+   if ! command -v yay &> /dev/null; then
+       echo "ERROR: you are using Arch or Arch based Linux, you need to install yay to continue"
+       exit 1
+   fi
+
+   sudo pacman --no-confirm -S python-pillow
+   sudo pacman --no-confirm -S tk
+   yay --noconfirm -S python-customtkinter
+fi
+
+echo "Installation completed successfully, to run the program run python main.py"
 
